d8d987f461b3b02959d5828575e5d20b333894fd
BIGPEL <bigpel66@gmail.com>
2021-09-28 16:41:36 +0900

    thread_pool with cv unq_lock future packaged_task template_type

---
+++ thread-pool/thread_pool.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread_pool.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/28 16:19:42 by jseo              #+#    #+#             */
+/*   Updated: 2021/09/28 16:37:57 by jseo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "thread_pool.hpp"
+
+using tp::thread_pool;
+
+void thread_pool::_run_thread()
+{
+	while (true)
+	{
+		std::unique_lock<std::mutex> lock(_m_jobs);
+		_cv_jobs.wait(lock, [this] () { return (!this->_jobs.empty() || this->_stop); } );
+		if (_stop && _jobs.empty())
+			return ;
+		std::function<void()> current = std::move(_jobs.front());
+		_jobs.pop();
+		lock.unlock();
+		current();
+	}
+}
+
+thread_pool::thread_pool()
+	: _stop(false), _num_threads(_default_size)
+{
+	_worker_threads.reserve(_num_threads);
+	for (size_t i = 0 ; i < _num_threads ; ++i)
+		_worker_threads.push_back(std::thread( [this] () { this->_run_thread(); } ));
+}
+
+thread_pool::thread_pool(size_t num_threads)
+	: _stop(false), _num_threads(num_threads)
+{
+	_worker_threads.reserve(_num_threads);
+	for (size_t i = 0 ; i < _num_threads ; ++i)
+		_worker_threads.push_back(std::thread( [this] () { this->_run_thread(); } ));
+}
+
+thread_pool::~thread_pool()
+{
+	_stop = true;
+	_cv_jobs.notify_all();
+	for (auto& t : _worker_threads)
+		t.join();
+}
---
+++ thread-pool/thread_pool.hpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread_pool.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/28 10:33:50 by jseo              #+#    #+#             */
+/*   Updated: 2021/09/28 12:20:29 by jseo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef THREAD_POOL_H
+# define THREAD_POOL_H
+
+# include <chrono>
+# include <condition_variable>
+# include <cstdio>
+# include <functional>
+# include <future>
+# include <mutex>
+# include <queue>
+# include <thread>
+# include <vector>
+
+namespace tp
+{
+	class thread_pool
+	{
+		private:
+			static const size_t						_default_size = 4;
+
+			bool									_stop;
+			size_t									_num_threads;
+			std::vector<std::thread>				_worker_threads;
+			std::condition_variable					_cv_jobs;
+			std::mutex								_m_jobs;
+			std::queue<std::function<void()> >		_jobs;
+
+			void _run_thread();
+
+		public:
+			thread_pool();
+			thread_pool(size_t num_threads);
+			~thread_pool();
+
+			template <typename F, typename... Args>
+			std::future<typename std::result_of<F(Args...)>::type> enque(F&& f, Args&&... args);
+	};
+
+	template <typename F, typename... Args>
+	std::future<typename std::result_of<F(Args...)>::type> thread_pool::enque(F&& f, Args&&... args)
+	{
+		if (_stop)
+			throw std::runtime_error("Thread Pool Stopped");
+		using return_type = typename std::result_of<F(Args...)>::type;
+		auto wrapped_job = std::make_shared<std::packaged_task<return_type()> >(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+		std::future<return_type> future_job = wrapped_job->get_future();
+		{
+			std::lock_guard<std::mutex> lock(_m_jobs);
+			_jobs.push([wrapped_job] () { (*wrapped_job)(); });
+		}
+		_cv_jobs.notify_one();
+		return (future_job);
+	}
+}
+
+#endif
