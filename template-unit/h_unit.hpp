313164d2c15ac4cf54299be7c5a0f82c6748b155
BIGPEL <bigpel66@gmail.com>
2021-09-29 19:46:59 +0900

    unit on ratio

---
+++ template-unit/h_unit.hpp
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   h_unit.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 17:28:33 by jseo              #+#    #+#             */
+/*   Updated: 2021/09/29 19:38:39 by jseo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UNIT_H
+# define UNIT_H
+
+# include "h_ratio.hpp"
+# include <iostream>
+
+template <typename U, typename V, typename W>
+class dimension
+{
+	public:
+		using X = U;
+		using Y = V;
+		using Z = W;
+		using type = dimension<U, V, W>;
+};
+
+template <typename D1, typename D2>
+class _dim_add
+{
+	public:
+		using type = dimension<
+						ratio_add<typename D1::X, typename D2::X>,
+						ratio_add<typename D1::Y, typename D2::Y>,
+						ratio_add<typename D1::Z, typename D2::Z>
+					>;
+};
+
+template <typename D1, typename D2>
+class dim_add : public _dim_add<D1, D2>::type {};
+
+template <typename D1, typename D2>
+class _dim_sub
+{
+	public:
+		using type = dimension<
+						ratio_sub<typename D1::Y, typename D2::Y>,
+						ratio_sub<typename D1::X, typename D2::X>,
+						ratio_sub<typename D1::Z, typename D2::Z>
+					>;
+};
+
+template <typename D1, typename D2>
+class dim_sub : public _dim_sub<D1, D2>::type {};
+
+template <typename T, typename D>
+class quantity
+{
+	public:
+		T t;
+		using dim_type = D;
+
+		quantity operator+(quantity<T, D> q)
+		{
+			return (quantity<T, D>(t + q.t));
+		}
+
+		quantity operator-(quantity<T, D> q)
+		{
+			return (quantity<T, D>(t - q.t));
+		}
+
+		template <typename C>
+		quantity<T, dim_add<D, C> > operator*(quantity<T, C> q)
+		{
+			return (quantity<T, dim_add<D, C> >(t * q.t));
+		}
+
+		template <typename C>
+		quantity<T, dim_sub<D, C> > operator/(quantity<T, C> q)
+		{
+			return (quantity<T, dim_sub<D, C> >(t / q.t));
+		}
+
+		quantity<T, D> operator*(T scalar)
+		{
+			return (quantity<T, D>(t * scalar));
+		}
+
+		quantity<T, D> operator/(T scalr)
+		{
+			return (quantity<T, D>(t / scalr));
+		}
+
+		quantity<T, D>& operator=(const quantity<T, D>& q)
+		{
+			t = q.t;
+			return (*this);
+		}
+
+		quantity(T t) : t(t) {};
+		quantity(const quantity<T, D>& q) = default;
+		quantity(quantity<T, D>&& q) = default;
+		~quantity() = default;
+};
+
+template <typename T, typename D>
+std::ostream& operator<<(std::ostream& out, const quantity<T, D>& q)
+{
+	out << q.t << "\tkg^" << D::X::num / D::X::den << "\tm^" << D::Y::num / D::Y::den << "\ts^" << D::Z::num / D::Z::den;
+	return (out);
+}
+
+#endif
--- template-unit/main.cpp
+++ template-unit/main.cpp
@@ -6,15 +6,18 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 16:53:28 by jseo              #+#    #+#             */
-/*   Updated: 2021/09/29 17:05:17 by jseo             ###   ########.fr       */
+/*   Updated: 2021/09/29 19:46:41 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "h_ratio.hpp"
-#include <iostream>
+#include "h_unit.hpp"
+
+using zero = ratio<0, 1>;
+using one = ratio<1, 1>;
 
 int main(void)
 {
+	/*
 	using r1 = ratio<2, 3>;
 	using r2 = ratio<3, 2>;
 	using r3 = ratio_mul<r1, r2>;
@@ -22,5 +25,56 @@ int main(void)
 
 	std::cout << "(2 / 3) * (3 / 2) = (" << r3::num << " / " << r3::den << ")\n";
 	std::cout << "(" << r3::num << " / " << r3::den << ") + " << "(15 / 6) = (" << r4::num << " / " << r4::den << ")\n";
+	*/
+
+	/*
+	quantity<double, dimension<one, zero, zero> > kg(1);
+	quantity<double, dimension<zero, one, zero> > m(1);
+	quantity<double, dimension<zero, zero, one> > s(1);
+
+	std::cout << kg << "\n";
+	std::cout << m << "\n";
+	std::cout << s << "\n";
+
+	decltype(kg) tmp1(3);
+	decltype(m) tmp2(4);
+	decltype(s) tmp3(5);
+
+	kg = tmp1;
+	m = tmp2;
+	s = tmp3;
+
+	std::cout << kg << "\n";
+	std::cout << m << "\n";
+	std::cout << s << "\n";
+
+	decltype(tmp1) tmp4(std::move(tmp1));
+	decltype(tmp2) tmp5(std::move(tmp2));
+	decltype(tmp3) tmp6(std::move(tmp3));
+
+	std::cout << tmp4 << "\n";
+	std::cout << tmp5 << "\n";
+	std::cout << tmp6 << "\n";
+
+	auto copy_kg(tmp4);
+	auto copy_m(tmp5);
+	auto copy_s(tmp6);
+
+	std::cout << copy_kg << "\n";
+	std::cout << copy_m << "\n";
+	std::cout << copy_s << "\n";
+	*/
+
+	using kg = quantity<double, dimension<one, zero, zero> >;
+	using m = quantity<double, dimension<zero, one, zero> >;
+	using s = quantity<double, dimension<zero, zero, one> >;
+
+	kg x(2);
+	m y(3);
+	s z(1);
+
+	auto value = x * y / (z * z);
+	std::cout << "Threw 2kg material accelerated by 3m/s^2\n\t" << value << "\n";
+
 	return (0);
 }
