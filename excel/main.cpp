e1de11de9756e17723ec5198966f59a2a4ae9384
BIGPEL <bigpel66@gmail.com>
2021-10-01 18:40:26 +0900

    circular including by forwarding declaration solved, excel table on

--- excel/main.cpp
+++ excel/main.cpp
@@ -6,20 +6,26 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 13:08:40 by jseo              #+#    #+#             */
-/*   Updated: 2021/09/30 23:55:14 by jseo             ###   ########.fr       */
+/*   Updated: 2021/10/01 18:39:06 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "h_cell.hpp"
-#include <iostream>
+#include "h_excel.hpp"
 
 int main(void)
-{
-	auto t = std::make_shared<table>();
-	auto d = std::make_shared<date_cell>("-2021-12-28", 0, 0, t);
-	auto e = std::make_shared<expr_cell>("3 + 4 * (5 + 4)) * (7 - 2)) * 100", 0 ,0, t);
 
-	std::cout << d->stringify() << "\n" << d->to_numeric() << "\n";
-	std::cout << e->stringify() << "\n" << e->to_numeric();
+{
+	auto t = std::make_shared<text_table>(5, 5);
+
+	auto d = std::make_shared<date_cell>("-2021-12-28", 0, 0, t);
+	t->reg_cell(d);
+	auto n = std::make_shared<numeric_cell>(3000.0, 2, 3, t);
+	t->reg_cell(n);
+	auto e = std::make_shared<expr_cell>("3 + 4 * (5 + 4) * (7 - 2) * 100", 1, 1, t);
+	t->reg_cell(e);
+	auto f = std::make_shared<expr_cell>("b1", 2, 2, t);
+	t->reg_cell(f);
+	std::cout << t.get();
+
 	return (0);
 }
